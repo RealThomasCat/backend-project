912039ca8c87ae12504d3e904e5f5d73b25a1660
Yashveer Singh <yashveer.singh@jethitech.com>
2023-12-20 22:51:32 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,11 +14,13 @@ app.use(
 // Three major configurations for the app
 
 app.use(express.json({ limit: "16kb" })); // JSON data is accepted with a limit of 16kb
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public")); // Stores static files in the public folder
-
 app.use(cookieParser()); // Allows the app to access and set cookies from user's browser
+
+import userRouter from "./routes/user.routes.js"; // Import routes
+
+// Routes declaration
+app.use("/api/v1/users", userRouter); // All routes in userRouter will be prefixed with /api/v1/users
 
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,23 +2,23 @@
 
 //USING PROMISES
 //
-// const asyncHandler = (requestHandler) => {
-//   (req, res, next) => {
-//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
 // USING TRY CATCH
 //
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-};
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
 
 export { asyncHandler };
