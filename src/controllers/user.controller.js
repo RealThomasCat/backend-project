87faa71ec380d03504671b546272d0092b732fd3
Yashveer Singh <yashveer.rtc@gmail.com>
2023-12-29 21:20:19 +0530

    Refactor getCurrentUser response to use ApiResponse class

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -304,7 +304,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "Current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -351,6 +351,8 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
       "Something went wrong while uploading avatar image"
     );
   }
+
+  // TO DO : Delete old avatar image from cloudinary
 
   // Update user avatar in database
   const user = await User.findByIdAndUpdate(
